fb16f725bf3dcfd6e333c79d7f9e42162d68ba92
Mihir <115444806+surf-excel@users.noreply.github.com>
2024-07-27 19:02:48 +0600

    1988A Split the Multiset.cpp

--- 1988A Split the Multiset.cpp
+++ 1988A Split the Multiset.cpp
@@ -1,21 +1,29 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define nline '\n'
+include <bits/stdc++.h>
 using namespace std;
-void solve(){
-    int n,k;
-    cin>>n>>k;
-    cout <<(n+k-3)/(k-1)<<nline;
+#define int long long int
+#define vi vector<int>
+#define read(a) for(auto &i: a) cin>>i
+#define mod 1000000007
+signed main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t=1;
+cin>>t;
+while(t--){
+int n;
+cin>>n;
+int k;
+cin>>k;
+if(n==1){
+cout<<0<<endl;
+continue;
 }
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    ll T;
-    cin>>T;
-    while (T--) 
-    {
-        solve();
-    }
-    return 0;
+int ans=0;
+k--;
+while(n>1){
+n=n-k;
+ans++;
 }
+cout<<ans<<endl;
+}
+return 0;}
