eb5c55be5064a6e9b6848ff370f21ba35d095cdf
Mihir <115444806+surf-excel@users.noreply.github.com>
2024-07-27 19:01:17 +0600

    Add files via upload

--- A_Diverse_Game.cpp
+++ A_Diverse_Game.cpp
@@ -1,32 +1,38 @@
-#include <cstdio>
-#include <vector>
-
-int main(){
-
-    long t; scanf("%ld", &t);
-    while(t--){
-        long n, m; scanf("%ld %ld", &n, &m);
-        std::vector<std::vector<long> > a(n, std::vector<long>(m, 0));
-        for(long row = 0; row < n; row++){for(long col = 0; col < m; col++){scanf("%ld", &a[row][col]);}}
-
-        for(long col = 0; col + 1 < m; col++){
-            long tmp = a[0][col];
-            a[0][col] = a[0][col + 1];
-            a[0][col + 1] = tmp;
-        }
-
-        for(long row = 0; row + 1 < n; row++){
-            std::vector<long> tmp = a[row];
-            a[row] = a[row + 1];
-            a[row + 1] = tmp;
-        }
-
-        if(n * m == 1){puts("-1"); continue;}
-
-        for(long row = 0; row < n; row++){
-            for(long col = 0; col < m; col++){printf("%ld ", a[row][col]);}
-            puts("");
-        }
-    }
-
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define vi vector<int>
+#define read(a) for(auto &i: a) cin >> i
+#define mod 1000000007
+#define nline '\n'
+signed main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin >> t;
+while (t--) {
+int n, m;
+cin >> n >> m;
+vector<vi> a(n, vi(m, 0));
+for (int row = 0; row < n; row++) {
+read(a[row]);
+}
+for (int col = 0; col + 1 < m; col++) {
+swap(a[0][col], a[0][col + 1]);
+}
+for (int row = 0; row + 1 < n; row++) {
+swap(a[row], a[row + 1]);
+}
+if (n * m == 1) {
+cout << "-1" << nline;
+continue;
+}
+for (const auto& row : a) {
+for (int val : row) {
+cout << val << " ";
+}
+cout << nline;
+}
+}
+return 0;
 }
