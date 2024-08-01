754a9fa17760401725ca3972de0d82d24a27455b
Mihir <115444806+surf-excel@users.noreply.github.com>
2024-08-01 21:50:00 +0600

    C_Sofia_and_the_Lost_Operations.cpp

--- C_Sofia_and_the_Lost_Operations.cpp
+++ C_Sofia_and_the_Lost_Operations.cpp
@@ -1,55 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+#define int long long int
 #define vi vector<int>
 #define read(a) for(auto &i: a) cin >> i
 #define mod 1000000007
 #define nline '\n'
 signed main() {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-int t=1;
-cin>>t;
-while(t--){
-int n; cin>>n;
-vi a(n);
-read(a);
-unordered_multiset<int> msd;
-        for (int i = 0; i < n; i++) {
-            int x;
-            cin >> x;
-            if (x != a[i]) {
-                msd.insert(x);
-            }
-        }
-        int m;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n;
+        vi a(n), b(n);
+        read(a);
+        read(b);
         cin >> m;
-        bool ans = true;
-        int d;
-        for (int p = 0; p < m; p++) {
-            cin >> d;
-            if (msd.size() > m - p || msd.empty()) {
-                continue;
-            }
-            auto it = msd.find(d);
-            if (it != msd.end()) {
-                msd.erase(it);
-            } else if (p + 1 == m) {
-                bool tmp = false;
-                for (int u = 0; u < n; u++) {
-                    if (d == a[u]) {
-                        tmp = true;
-                        break;
-                    }
-                }
-                ans = tmp;
+        vi d(m);
+        map<int, int> mp;
+        for (int i = 0; i < m; ++i) {
+            cin >> d[i];
+            mp[d[i]]++;
+        }
+        bool f = true;
+        for (int i = 0; i < n; ++i) {
+            if (a[i] == b[i]) continue;
+            if (mp[b[i]]) {
+                mp[b[i]]--;
+            } else {
+                f = false;
             }
         }
-        if (msd.empty() && ans) {
-            cout << "YES" << nline;
-        } else {
-            cout << "NO" << nline;
-        }
+        if (count(b.begin(), b.end(), d[m - 1]) == 0) f = false;
+        if (f) cout << "YES" << nline;
+        else cout << "NO" << nline;
     }
+
     return 0;
 }
